81dbd14c9f354a86a952977d317377f3cf61cb82
Veta <vetarara@yandex.ru>
2026-06-11 06:48:30 +0300

    fix menu and links

--- source/scripts/menu.js
+++ source/scripts/menu.js
@@ -1,5 +1,6 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
+const body = document.body;
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -7,8 +8,25 @@ navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+
+    body.classList.add('no-scroll');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+
+    body.classList.remove('no-scroll');
   }
+});
+
+const navLinks = document.querySelectorAll('.main-nav a');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    if (navMain.classList.contains('main-nav--opened')) {
+      navMain.classList.remove('main-nav--opened');
+      navMain.classList.add('main-nav--closed');
+
+      body.classList.remove('no-scroll');
+    }
+  });
 });
